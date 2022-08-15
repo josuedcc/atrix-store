e327fe2e0cfde7fbbb011433a3a13363da5eff11
Josué Daniel Contreras Cucchi <josuedcc@icloud.com>
2022-08-15 02:05:45 -0500

    Initial commit

---
+++ menu.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="assets/css/style-atrix.css">
+    <title>Atrix Store</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-sm">
+        <!-- Brand/logo -->
+        <div class="marco-logo">
+            <a class="navbar-brand" href="#">
+                <img id="logo" src="assets/images/logo/logo.png" srcset="assets/images/logo/logo.png 1x, assets/images/logo@2x.png 2x">
+            </a>
+        </div>
+        
+        <!-- Links -->
+
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Productos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="vistaEmpleado.php">Nosotros</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="vistaProductos.php">Contactenos</a>
+            </li>
+            
+        </ul>
+
+    </nav>
